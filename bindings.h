947b82e75803a0820a4b8e9ee669cc72a70eddcb
Andreas Monitzer <a.monitzer@dungeonfog.com>
2023-07-02 22:07:31 +0200

    Implemented using the display.

---
+++ bindings.h
@@ -0,0 +1,8 @@
+#include ".embuild/espressif/esp-idf/release-v5.1/components/esp_lcd/include/esp_lcd_types.h"
+#include ".embuild/espressif/esp-idf/release-v5.1/components/esp_lcd/include/esp_lcd_panel_rgb.h"
+#include ".embuild/espressif/esp-idf/release-v5.1/components/esp_lcd/include/esp_lcd_panel_io.h"
+#include ".embuild/espressif/esp-idf/release-v5.1/components/esp_lcd/include/esp_lcd_panel_ops.h"
+#include ".embuild/espressif/esp-idf/release-v5.1/components/esp_lcd/include/esp_lcd_panel_vendor.h"
+#include ".embuild/espressif/esp-idf/release-v5.1/components/esp_lcd/include/esp_lcd_panel_commands.h"
+#include ".embuild/espressif/esp-idf/release-v5.1/components/esp_lcd/interface/esp_lcd_panel_interface.h"
+#include ".embuild/espressif/esp-idf/release-v5.1/components/esp_lcd/interface/esp_lcd_panel_io_interface.h"
